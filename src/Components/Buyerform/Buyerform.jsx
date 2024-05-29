5ae21513647830ae82c3622c9b38bb34a4818f7a
Sinan-muhammed <sinasinan418@gmail.com>
2024-05-29 10:52:35 +0530

    first commit

--- src/Components/Buyerform/Buyerform.jsx
+++ src/Components/Buyerform/Buyerform.jsx
@@ -6,6 +6,14 @@ const BuyerForm = ()=>{
         <>
           <div className="buyerForm  w-full h-full bg-cover ">
                       <img src="../../../public/Bgimage.png" alt="" className='w-[100%] h-[100%] object-cover '/>
+                      <div className="heading absolute top-80 left-[20%] flex flex-col items-center justify-center h-9">
+                        <h1 className='text-[55px] font-[650] text-white font-Poppins'>
+                            Are You a Supplier?
+                        </h1>
+                        <h1 className='text-[65px] font-[300] text-white font-Poppins'>
+                             Explore Matching Opportunities.
+                        </h1>
+                    </div>
           </div>
         </>
     )
